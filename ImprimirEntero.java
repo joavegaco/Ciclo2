1ac6e0ad1eddb44f00a3df4f12dd2037be61d7e3
joavegaco <joavegaco311@gmail.com>
2021-09-13 07:03:41 -0500

    a

---
+++ ImprimirEntero.java
@@ -0,0 +1,10 @@
+public class ImprimirEntero {
+    public static void main(String args[]) {
+      
+{
+	double c = 1.4 + 1.6;
+	System.out.println("La nota es: " + (int)c);
+	// Cambia a entero
+}        
+    }
+}
